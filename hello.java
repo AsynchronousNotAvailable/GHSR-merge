8a1299c3e1b4ec0bd891ec122585abd10328f569
fyiernzy <fyiernzy@gmail.com>
2023-12-04 10:53:33 +0800

    first update from main

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class hello{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.print(sayHelloFromMain());
+    }
+
+    static String sayHelloFromMain(){
+        return new String("hi from main");
     }
 }
