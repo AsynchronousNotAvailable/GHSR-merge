b0764c92356a428d46e25c4ccc220bf09f0cf719
fyiernzy <fyiernzy@gmail.com>
2023-12-04 10:51:30 +0800

    first update from new branch

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class hello{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.print(sayHelloFromNewBranch());
+    }
+
+    static String sayHelloFromNewBranch(){
+        return new String ("Hello from new branch");
     }
 }
